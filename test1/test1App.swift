73300efaf839a8e71b851e4989734fcb8693ec8e
Sobko Sofiya <s.sobko@neurone.ru>
2022-10-17 10:50:50 +0300

    Initial Commit

---
+++ test1/test1App.swift
@@ -0,0 +1,17 @@
+//
+//  test1App.swift
+//  test1
+//
+//  Created by Sofia Sobko on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
